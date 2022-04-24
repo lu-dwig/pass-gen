e6e5ad2274cf8cd824700d0afa96d0c9e3854579
ludwig <murimiludwig@gmail.com>
2022-04-24 16:50:07 +0300

    this will add the check credentials class that checks the credentials on the credentials list and returns true or false depending on the status of the credentials

--- passlock.py
+++ passlock.py
@@ -76,11 +76,19 @@ class Credentials():
         for credential in cls.credentials_list:
             if credential.account == account:
                 return credential
+            
     @classmethod
     def copy_password(cls,account):
         found_credentials = Credentials.find_credentials(account)
         pyperclip.copy(found_credentials.password)
         
-        
-    
+    @classmethod
+    def if_credentials_exist(cls, account):
+        """
+        method that checks if a credential exists from the credentials list and returns true or false depending if the credential exists.
+        """
+        for credential in cls.credentials_list:
+            if credential.account == account:
+                return True
+        return False
                 
