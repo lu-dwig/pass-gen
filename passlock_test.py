6866e3d5a53578486abc8b2d17797d950c9ae94b
ludwig <murimiludwig@gmail.com>
2022-04-24 21:17:43 +0300

    this will add the test class for credentials

--- passlock_test.py
+++ passlock_test.py
@@ -10,4 +10,30 @@ class TestClass(unittest.TestCase):
         """
         Method that runs before each individual test methods run.
         """
-        self.new_user = User('Ludwigmurimi','XyZ3thf1')
+        self.new_user = User('LudwigMurimi','XyZ3thf1')
+        
+    def test_init(self):
+        """
+        test case to chek if the object has been initialized correctly
+        """
+        self.assertEqual(self.new_user.username,'LudwigMurimi')
+        self.assertEqual(self.new_user.password,'XyZ3thf1')
+        
+    def test_save_user(self):
+        """
+        test case to test if a new user instance has been saved into the User list
+
+        """
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+class TestCredentials(unittest.TestCase):
+    """
+    A test class that defines test cases for credentials class
+
+    """ 
+    def setUp(self):
+        """
+        Method that runs before each individual credentials test methods run.
+
+        """
+        self.new_credential = Credentials('Gmail','Ludwig_murimi','yx5Gij43') 
