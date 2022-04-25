f7c24fce4aed2c6b9caa68018d2d8f5737b0c760
ludwig <murimiludwig@gmail.com>
2022-04-25 06:36:27 +0300

    this will add the function that saves, displays and login user

--- interface.py
+++ interface.py
@@ -17,4 +17,24 @@ def create_new_user(username,password):
     new_user = User(username,password)
     return new_user
 
+def save_user(user):
+    '''
+    Function to save a new user
+    '''
+    user.save_user()
+    
+def display_user():
+    """
+    Function to display existing user
+    """
+    return User.display_user()
+
+def login_user(username,password):
+    """
+    function that checks whether a user exist and then login the user in.
+    """
+  
+    check_user = Credentials.verify_user(username,password)
+    return check_user
+
 
